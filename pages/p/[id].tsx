de25e462eb40fa10c0b3997466f94fc939fd210c
@chiennguyenduc <chiennguyenduc@channelvn.net>
2023-11-15 05:44:12 +0700

    update

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -55,12 +55,11 @@ const Post: React.FC<PostProps> = (props) => {
         <h2>{title}</h2>
         <p>By {props?.author?.name || "Unknown author"}</p>
         <ReactMarkdown children={props.content} />
-        {!props.published && userHasValidSession && postBelongsToUser && (
+        
           <button onClick={() => publishPost(props.id)}>Publish</button>
-        )}
-        {userHasValidSession && postBelongsToUser && (
+     
           <button onClick={() => deletePost(props.id)}>Delete</button>
-        )}
+       
       </div>
       <style jsx>{`
         .page {
